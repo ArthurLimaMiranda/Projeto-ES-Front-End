b30ac402d208885ad9b31e6868ee837e87630fad
Mayanne Gomes <mayane.viviane0511@gmail.com>
2024-05-18 09:45:02 -0300

    add icons + finalização da pag Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,8 @@ import background from '../assets/bg2.jpg';
 import Image from 'next/image';
 import { Header } from "./Header";
 import { DashClientes } from './DashClientes';
+import {BiUser} from 'react-icons/bi';
+import {AiOutlineLock} from 'react-icons/ai';
 
 export function Login() {
     return (
@@ -22,26 +24,23 @@ export function Login() {
                 <div className="relative my-4">
                     <input 
                         type="email" 
-                        className="peer border-gray-300 rounded-md px-0 py-2.3 w-72 block text-sm text-white bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#4BC3B7]" 
-                        placeholder=" " 
-                    />
-                    <label 
-                        className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                        E-mail
-                </label>
+                        className="peer border-gray-300 rounded-md px-0 py-2.3 w-72 block text-sm text-white bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#4BC3B7]" placeholder=" " />
+                    <label className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">E-mail</label>
+                    <BiUser className='absolute top-4 right-4 text-white'/>
                 </div>
                 <div className="relative my-4">
                     <input 
                         type="email" 
                         className="peer border-gray-300 rounded-md px-0 py-2.3 w-72 block text-sm text-white bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-[#4BC3B7]" placeholder=" "/>
                     <label className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Senha</label>
-                    </div>
+                    <AiOutlineLock className='absolute top-4 right-4 text-white'/>
+                </div>
                     <div className='justify-center items-center text-center'>
                         <span className="ml-auto text-sm text-white hover:text-[#4BC3B7] hover:underline"><a href='forgotpassword'>Esqueceu a senha?</a></span>
                     </div>
                     <button type="submit" className='w-full mb-4 text-[18px] mt-6 rounded-full border-4 border-[#4BC3B7] px-4 bg-transparent text-white hover:bg-[#F5F5F5] hover:border-[#F5F5F5] hover:text-[#03494E] py-2 transition-colors duration-300'>Login</button>
                     <div className="flex mb-4 gap-1.5 items-center">
-                            <input type="checkbox" name="" id="" className="mr-2" />
+                            <input type="checkbox" name="" id="" className='mr-2 appearance-none border-[#03494E] rounded-sm focus:ring-1 focus:ring-[#4BC3B7] checked:bg-[#4BC3B7]'  />
                             <label htmlFor='' className='text-white'>Lembre-se de mim</label>
                         </div>
                     <div className="mt-4 ">
